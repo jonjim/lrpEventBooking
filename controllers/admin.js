bb4d1c25e085b9ce15306903050ce18de13f4dcd
Jonathan Kane <jonjim@hotmail.co.uk>
2026-01-20 18:23:57 +0000

    Removed comments

--- controllers/admin.js
+++ controllers/admin.js
@@ -202,7 +202,6 @@ module.exports.eventSystemDelImg = async(req, res, next) => {
 
 module.exports.eventSystemUpdate = async (req, res, next) => {
     req.body.active = typeof req.body.active != 'undefined' ? req.body.active == 'on' ? true : false : false;
-    console.log(req.body)
     if (req.body.customTools && req.body.customTools != '') req.body.customTools = req.body.customTools.split(',');
     else req.body.customTools = undefined;
     const eventSystem = await EventSystems.findByIdAndUpdate(req.params.id, {...req.body }, { new: true });
